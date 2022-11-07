67c9bd0d58604eaddce2511c1c3d755eb80cc3ab
はらだ <65936854+hankakukatakana@users.noreply.github.com>
2022-11-07 23:00:18 +0900

    poi

--- db.py
+++ db.py
@@ -1,8 +1,11 @@
 import sqlite3
 
 dbname = 'lifeline.db'
-
 conn = sqlite3.connect(dbname)
+
+cur = conn.cursor()
+
 conn = execute("CREATE TABLE lifeline(mm, category, prise)")
+#cur.execute('INSENT INTO lifeline values()')
 
 conn = close()
