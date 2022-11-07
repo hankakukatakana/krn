384c3bf76b8d474696f4a2066de82d6c1cef65af
hankakukatakana <masaki.0409.a@gmail.com>
2022-11-08 02:38:39 +0900

    ok

--- flasker/db.py
+++ flasker/db.py
@@ -1,11 +1,7 @@
 import sqlite3
 
 dbname = 'lifeline.db'
-con = sqlite3.connect(dbname)
-
-cur = con.cursor()
-
-con.execute("CREATE TABLE lifeline(mm, category, prise)")
-#cur.execute('INSENT INTO lifeline values()')
-
-con.close()
+def create_table():
+    con = sqlite3.connect(dbname)
+    con.execute("CREATE TABLE IF NOT EXISTS lifeline (mm, category, price)")
+    con.close()
---
+++ flasker/main.py
@@ -0,0 +1,52 @@
+from flasker import app
+from flask import render_template, request, redirect, url_for
+import sqlite3
+dbname = 'lifeline.db'
+
+@app.route("/")
+def home():
+    con = sqlite3.connect(dbname)
+    db_lifeline = con.execute('SELECT * FROM lifeline').fetchall()
+    con.close()
+
+    lifelines = []
+    for row in db_lifeline:
+        lifelines.append({'mm':row[0], 'category':row[1], 'prise':row[2]})
+
+    return render_template('home.html', lifelines=lifelines)
+
+@app.route('/gas')
+def gas():
+    name = 'Gas'
+    return render_template('category.html', name=name)
+
+@app.route('/denki')
+def denki():
+    name = 'Denki'
+    return render_template('category.html', name=name)
+
+@app.route('/suidou')
+def suidou():
+    name = 'Suidou'
+    return render_template('category.html', name=name)
+
+@app.route('/form')
+def form():
+    name = 'form'
+    return render_template('form.html', name=name)
+
+@app.route('/register', methods=['POST'])
+def register():
+    mm = request.form['mm']
+    category = request.form['category']
+    price = request.form['price']
+
+    con = sqlite3.connect(dbname)
+    con.execute('INSERT INTO lifeline VALUES(?, ?, ?)',
+                [mm, category, price])
+    con.commit()
+    con.close()
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(debug=True, host='localhost', port=5000)
