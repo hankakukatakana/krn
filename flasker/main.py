986479752e4075aab96e77d7a6a54be4ae00f8df
hankakukatakana <masaki.0409.a@gmail.com>
2022-11-08 05:10:41 +0900

    これ

--- flasker/main.py
+++ flasker/main.py
@@ -1,10 +1,14 @@
 from flasker import app
 from flask import render_template, request, redirect, url_for
+from datetime import datetime
 import sqlite3
+
 dbname = 'lifeline.db'
 
 @app.route("/")
 def home():
+    today = datetime.today()
+    mm = today.month
     con = sqlite3.connect(dbname)
     db_lifeline = con.execute('SELECT * FROM lifeline').fetchall()
     con.close()
@@ -13,11 +17,19 @@ def home():
     for row in db_lifeline:
         lifelines.append({'mm':row[0], 'category':row[1], 'prise':row[2]})
 
-    return render_template('home.html', lifelines=lifelines)
+    return render_template('home.html', lifelines=lifelines, mm=mm)
 
 @app.route('/gas')
 def gas():
     name = 'Gas'
+    con = sqlite3.connect(dbname)
+    db_element = con.execute("SELECT * FROM lifeline WHERE category = 'ガス'").fetchall()
+    con.close()
+
+    elements = []
+    for row in db_element:
+        elements.append({'mm':row[0], 'category':row[1], 'prise':row[2]})
+    
     return render_template('category.html', name=name)
 
 @app.route('/denki')
